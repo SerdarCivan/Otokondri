a82e4fe6e547e80aff87c808fcd753e0fb9d8ca1
SerdarCivan <51891037+SerdarCivan@users.noreply.github.com>
2019-06-30 02:40:46 +0300

    Update AracDetayDuzenle.cs

--- AracDetayDuzenle.cs
+++ AracDetayDuzenle.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,16 +18,20 @@ namespace Otokondri
             InitializeComponent();
         }
 
+        public string personelid;
+
         
 
         private void AracDetayDuzenle_geri_Click(object sender, EventArgs e)
         {
             PersonelPanel PersonelPanel = new PersonelPanel();
+            //PersonelPanel.Show();
             this.Hide();
         }
 
         private void AracDetayDuzenle_Load(object sender, EventArgs e)
         {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
             SqlConnection con = new SqlConnection("Data Source=DESKTOP-7L6TOHU;Initial Catalog=OtokondriOtomasyon;Persist Security Info=True;User ID=sa;Password=123456789");
             con.Open();
             SqlDataAdapter adapter = new SqlDataAdapter("Select * from tbl_Arac", con);
@@ -35,6 +39,10 @@ namespace Otokondri
             adapter.Fill(dtable);
             dataGridView1.DataSource = dtable;
             con.Close();
+            this.dataGridView1.Columns["Id"].Visible = false;
+            this.dataGridView1.Columns["Satis_Durumu"].Visible = false;
+            this.dataGridView1.Columns["Kar"].Visible = false;
+            this.dataGridView1.Columns["Zarar"].Visible = false;
         }
 
         private void dataGridView1_MouseDoubleClick(object sender, MouseEventArgs e)
@@ -50,6 +58,8 @@ namespace Otokondri
             aracgnc.id = dataGridView1["Id", dataGridView1.CurrentCell.RowIndex].Value.ToString();
             aracgnc.alistarihi = dataGridView1["Alis_Tarihi", dataGridView1.CurrentCell.RowIndex].Value.ToString();
             aracgnc.satistarihi = dataGridView1["Satis_Tarihi", dataGridView1.CurrentCell.RowIndex].Value.ToString();
+            aracgnc.satis_durum = dataGridView1["Satis_Durumu", dataGridView1.CurrentCell.RowIndex].Value.ToString();
+            aracgnc.personelid = personelid;
 
             string sorgu= "select * from tbl_Fotograf where Arac_Id="+ dataGridView1["Id", dataGridView1.CurrentCell.RowIndex].Value.ToString();
             aracgnc.resimtable= SqlConn.goster(sorgu);
