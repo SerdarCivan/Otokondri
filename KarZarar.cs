b53cdd44284218de0116bdd22f9ff95118707325
SerdarCivan <51891037+SerdarCivan@users.noreply.github.com>
2019-06-30 02:40:00 +0300

    Add files via upload

---
+++ KarZarar.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
+
+namespace Otokondri
+{
+    public partial class KarZarar : Form
+    {
+        public KarZarar()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Listele_Click(object sender, EventArgs e)
+        {
+            
+
+            try
+            {
+                string tarih1 = dateTimePicker1.Text;
+                string tarih2 = dateTimePicker2.Text;
+
+                string sorgu = @"select Marka,Model,Alis_Fiyati,Satis_Fiyati,Alis_Tarihi,Satis_Tarihi,Kar,Zarar
+                             from tbl_Arac 
+                             where convert(datetime,Satis_Tarihi,104) between convert(datetime,'"+tarih1+"',104) and convert(datetime,'"+tarih2+"',104) AND Satis_Durumu = 1";
+                System.Data.DataTable table = SqlConn.goster(sorgu);
+                dataGridView1.DataSource = table;
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Hata!!");
+            }
+        }
+        void excele_aktar(DataGridView dg)
+        {
+            dg.AllowUserToAddRows = false;
+            System.Globalization.CultureInfo dil = System.Threading.Thread.CurrentThread.CurrentCulture;
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-us");
+            Microsoft.Office.Interop.Excel.Application Tablo = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook kitap = Tablo.Workbooks.Add(true);
+            Microsoft.Office.Interop.Excel.Worksheet sayfa = (Microsoft.Office.Interop.Excel.Worksheet)Tablo.ActiveSheet;
+            System.Threading.Thread.CurrentThread.CurrentCulture = dil;
+            Tablo.Visible = true;
+            sayfa = (Worksheet)kitap.ActiveSheet;
+            for (int i = 0; i < dg.Rows.Count; i++)
+            {
+                for (int j = 0; j < dg.ColumnCount; j++)
+                {
+                    if (i == 0)
+                    {
+                        Tablo.Cells[1, j + 1] = dg.Columns[j].HeaderText;
+                    }
+                    Tablo.Cells[i + 2, j + 1] = dg.Rows[i].Cells[j].Value.ToString();
+                }
+            }
+            Tablo.Visible = true;
+            Tablo.UserControl = true;
+        }
+
+        private void button_excel_aktar_Click(object sender, EventArgs e)
+        {
+            Excel.Application excel = new Excel.Application();
+            excel.Visible = true;
+            object Missing = Type.Missing;
+            Workbook workbook = excel.Workbooks.Add(Missing);
+            Worksheet sheet1 = (Worksheet)workbook.Sheets[1];
+            int StartCol = 1;
+            int StartRow = 1;
+            for (int j = 0; j < dataGridView1.Columns.Count; j++)
+            {
+                Range myRange = (Range)sheet1.Cells[StartRow, StartCol + j];
+                myRange.Value2 = dataGridView1.Columns[j].HeaderText;
+            }
+            StartRow++;
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+
+                    Range myRange = (Range)sheet1.Cells[StartRow + i, StartCol + j];
+                    myRange.Value2 = dataGridView1[j, i].Value == null ? "" : dataGridView1[j, i].Value;
+                    myRange.Select();
+
+
+                }
+            }
+        }
+
+        private void button_Karzarar_geri_Click(object sender, EventArgs e)
+        {
+            Raporlar rpr = new Raporlar();
+            rpr.Show();
+            this.Hide();
+        }
+
+        private void KarZarar_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+    }
+}
---
+++ Raporlar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Otokondri
+{
+    public partial class Raporlar : Form
+    {
+        public Raporlar()
+        {
+            InitializeComponent();
+        }
+
+        private void button_karzarar_Click(object sender, EventArgs e)
+        {
+            KarZarar zarar = new KarZarar();
+            zarar.Show();
+            this.Hide();
+        }
+        
+
+        private void button_markabaz_Click(object sender, EventArgs e)
+        {
+            MarkaBaz mrkbaz = new MarkaBaz();
+            mrkbaz.Show();
+            this.Hide();
+        }
+
+        private void Raporlar_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+    }
+}
